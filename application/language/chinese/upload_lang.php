c25a4b46e26e1628f9d742f54e0eac8a664dc0df
liangqifeng <695745674@qq.com>
2014-06-18 21:12:23 +0800

    lang

---
+++ application/language/chinese/upload_lang.php
@@ -0,0 +1,9 @@
+﻿<?php
+
+$lang['upload_no_file_selected']				='请选择上传文件';
+/*$lang['upload_file_exceeds_limit']				='请选择上传文件';
+$lang['upload_file_exceeds_form_limit']				='请选择上传文件';
+$lang['upload_file_partial']				='请选择上传文件';
+$lang['upload_no_temp_directory']				='无temp文件夹';
+$lang['upload_unable_to_write_file']				='写文件失败';
+$lang['upload_stopped_by_extension']				='请选择上传文件';*/
