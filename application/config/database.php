d48a04c2f586ab9e26c0bcd084badcda7ef97cdc
5842228230 <hi@qonde-v.com>
2015-01-14 19:57:30 +0800

    t

--- application/config/database.php
+++ application/config/database.php
@@ -42,6 +42,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
+//$db['default']['hostname'] = '58.42.228.230';
+//$db['default']['username'] = 'tit';
+//$db['default']['password'] = 'kairen1048';
 //$db['default']['username'] = 'kwapro_user';
 //$db['default']['password'] = 'kwaprokendi851212';
 $db['default']['username'] = 'root';
